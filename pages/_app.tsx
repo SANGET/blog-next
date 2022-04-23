79c2004ec2f723dfb69479d74c5671f12c1ace05
SANGET <3348592+SANGET@users.noreply.github.com>
2022-04-23 22:24:21 +0800

    Add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { initRequest } from "@/services/req";
+import Script from "next/script";
 import { useEffect, useState } from "react";
 import "../styles/globals.css";
 import "../styles/style.scss";
@@ -12,6 +13,23 @@ function MyApp({ Component, pageProps }) {
       setIsReady(true);
     });
   }, []);
-  return isReady ? <Component {...pageProps} /> : <div></div>;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-125030746-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-125030746-1');
+        `}
+      </Script>
+      {isReady ? <Component {...pageProps} /> : <div></div>}
+    </>
+  );
 }
 export default MyApp;
